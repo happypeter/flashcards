9267830dc54f075b84b59364c5072ba3907bd8e5
Peter Wang <happypeter1983@gmail.com>
2022-12-27 15:49:57 +0800

    css

--- scripts/build.py
+++ scripts/build.py
@@ -1,13 +1,51 @@
 #!/usr/bin/env python
+import shutil
 
-L = """
-hello
-peter
-<div>
+shutil.copy("main.css", "../_cards/main.css")
+
+
+header = """
+<html>
+<head>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+"""
+
+footer = """
+</body>
+</html>
+"""
+
+file2 = open('../words.md', 'r')
+
+Lines = file2.readlines()
+  
+count = 0
+
+temp = """
+<div class="card">
+  <div class="uid">
+    {}
+  </div>
+  <img src="./img/{}.jpeg" />
+  <div class="word">
+    {}
+  </div>
 </div>
 """
 
+cards = ''
+for line in Lines:
+    count += 1
+    uid = line.strip().split()[0]
+    word = line.strip().split()[1]
+    cards = cards + temp.format(uid, uid, word)
+  
+
+content = header + cards + footer
+
 # writing to file
-file1 = open('index.html', 'w')
-file1.write(L)
+file1 = open('../_cards/index.html', 'w')
+file1.write(content)
 file1.close()
